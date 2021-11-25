45d3a92e422a8d5d19d137752905b91d8ce69192
aflahur <aflahurrofik@gmail.com>
2021-11-25 21:03:49 +0700

    pokemon react using pokemon api

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function Navbar() {
+    return (
+        <div className='Navbar'>
+            POKEMON USING REACT
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/helper/pokemonTypes.js
@@ -0,0 +1,43 @@
+const typeColors = {
+
+    bug: '#729f3f',
+
+    dragon: '#53a4cf',
+
+    fairy: '#fdb9e9',
+
+    fire: '#fd7d24',
+
+    ghost: '#7b62a3',
+
+    ground: '#f7de3f',
+
+    normal: '#a4acaf',
+
+    pyshic: '#f366b9',
+
+    steel: '#9eb7',
+
+    dark: '#707070',
+
+    electric: '#eed535',
+
+    fighting: '#d56723',
+
+    flying: '#3dc7ef',
+
+    grass: '#9bcc50',
+
+    ice: '#51c4e7',
+
+    poison: '#b92fc9',
+
+    rock: '#a38c21',
+
+    water: '#4592c4'
+
+}
+
+
+
+export default typeColors;
